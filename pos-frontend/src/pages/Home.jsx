f1ce827348cfeeeb5ef9312130725a78400291e2
amitesh08 <aamitesh125@gmail.com>
2025-07-29 20:36:09 +0530

    feat: home page UI Greeting and stats Cards

--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import BottomNav from "../components/shared/BottomNav";
+import Greetings from "../components/home/Greetings";
+import MiniCard from "../components/home/MiniCard";
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
 
 const Home = () => {
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
       {/* left div */}
-      <div className="flex-[3] bg-[#1a1a1a]"></div>
+      <div className="flex-[3]">
+        <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard
+            title="Total Earnings"
+            icons={<BsCashCoin />}
+            number={512}
+            footerNum={1.6}
+          />
+          <MiniCard
+            title="Total Earnings"
+            icons={<GrInProgress />}
+            number={16}
+            footerNum={3.6}
+          />
+        </div>
+      </div>
+
       {/* Right div */}
       <div className="flex-[2] bg-[#1a1a1a]"></div>
       <BottomNav />

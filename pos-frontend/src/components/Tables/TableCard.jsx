995271289721bb5463fbce897f009f5e6d81fc4c
amitesh08 <aamitesh125@gmail.com>
2025-08-03 16:11:34 +0530

    feat: added table card

---
+++ pos-frontend/src/components/Tables/TableCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { getRandomBG } from "../../utils";
+
+const TableCard = ({ key, name, status, initials }) => {
+  return (
+    <div
+      key={key}
+      className="w-[250px] hover:bg-[#2c2c2c] bg-[#262626] p-4 rounded-lg  cursor-pointer "
+    >
+      <div className="flex items-center justify-between px-1">
+        <h1 className="text-[#f5f5f5] text-lg font-semibold">{name}</h1>
+        <p
+          className={`${
+            status === "Booked"
+              ? "text-green-600 bg-[#2e4a40]"
+              : "bg-[#937019] text-white "
+          } px-2 py-1 rounded-lg`}
+        >
+          {status}
+        </p>
+      </div>
+      <div className="flex items-center justify-center mt-5 mb-7">
+        <h1
+          className={`${getRandomBG()} w-[60px] h-[60px] text-[#f5f5f5] rounded-full p-4 text-xl `}
+        >
+          {initials}
+        </h1>
+      </div>
+    </div>
+  );
+};
+export default TableCard;

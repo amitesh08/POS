22beb35b7306fe86d16ccad037ef2b185d15ca86
amitesh08 <aamitesh125@gmail.com>
2025-08-21 22:25:39 +0530

    feat: menu page completed with cart and items and billing info

--- pos-frontend/src/components/menu/MenuContainer.jsx
+++ pos-frontend/src/components/menu/MenuContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { menus } from "../../constants";
 import { GrRadialSelected } from "react-icons/gr";
+import { FaShoppingCart } from "react-icons/fa";
 
 const MenuContainer = () => {
   const [selected, setSelected] = useState(menus[0]);
@@ -69,12 +70,15 @@ const MenuContainer = () => {
           return (
             <div
               key={menu.id}
-              className="flex flex-col items-start justify-between p-4 rounded-lg h-[120px] cursor-pointer bg-[#1a1a1a] hover:bg-[#2a2a2a]"
+              className="flex flex-col items-start justify-between p-4 rounded-lg h-[150px] cursor-pointer bg-[#1a1a1a] hover:bg-[#2a2a2a]"
             >
               <div className="flex items-center justify-between w-full ">
                 <h1 className="text-[#f5f5f5] text-lg font-semibold ">
                   {menu.name}
                 </h1>
+                <button className="bg-[#2e4a40] text-[#02ca3a] cursor-pointer p-2 rounded-lg">
+                  <FaShoppingCart size={20} />
+                </button>
               </div>
               <div className="flex items-center justify-between w-full ">
                 <p className="text-[#f5f5f5] text-xl font-bold">
--- pos-frontend/src/pages/Menu.jsx
+++ pos-frontend/src/pages/Menu.jsx
@@ -3,10 +3,13 @@ import BottomNav from "../components/shared/BottomNav";
 import BackButton from "../components/shared/BackButton";
 import { MdRestaurantMenu } from "react-icons/md";
 import MenuContainer from "../components/menu/MenuContainer";
+import CustomerInfo from "../components/menu/CustomerInfo";
+import CartInfo from "../components/menu/CartInfo";
+import Bill from "../components/menu/Bill";
 
 const Menu = () => {
   return (
-    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+    <section className="bg-[#1f1f1f] h-[calc(100vh-7rem)] overflow-y-scroll scrollbar-hide flex gap-3">
       {/* left div */}
       <div className="flex-[3]">
         <div className="flex items-center justify-between px-10 py-4 ">
@@ -34,7 +37,15 @@ const Menu = () => {
       </div>
 
       {/* Right div */}
-      <div className="flex-[1] bg-blue-400"></div>
+      <div className="flex-[1] bg-[#1a1a1a] mt-4 mr-3 h-[780px] rounded-lg pt-2">
+        {/*Customer Info */}
+        <CustomerInfo />
+        <hr className="border-[#2a2a2a] border-t-2 " />
+        {/* Cart Items */}
+        <CartInfo />
+        {/* Bills */}
+        <Bill />
+      </div>
       <BottomNav />
     </section>
   );

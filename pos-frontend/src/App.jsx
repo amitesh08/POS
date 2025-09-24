f8d10318bfb9fd5a38c6ec2f6b8503a2ab4c5a9b
amitesh08 <aamitesh125@gmail.com>
2025-09-24 22:08:43 +0530

    feat: added protected routes and added loader

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -3,28 +3,79 @@ import {
   Routes,
   Route,
   useLocation,
+  Navigate,
 } from "react-router-dom";
 import { Auth, Home, Menu, Orders, Tables } from "./pages";
 import Header from "./components/shared/header";
+import { useSelector } from "react-redux";
+import useLoadData from "./hooks/useLoadData";
+import FullScreenLoader from "./components/shared/FullScreenLoader";
 
 function Layout() {
   const location = useLocation();
-
+  const isLoading = useLoadData();
   const hideHeaderRoutes = ["/auth"];
+  const { isAuth } = useSelector((state) => state.user);
+
+  if (isLoading) return <FullScreenLoader />;
 
   return (
     <>
       {!hideHeaderRoutes.includes(location.pathname) && <Header />}
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/auth" element={<Auth />}></Route>
-        <Route path="/orders" element={<Orders />}></Route>
-        <Route path="/tables" element={<Tables />}></Route>
-        <Route path="/menu" element={<Menu />}></Route>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        ></Route>
+
+        <Route
+          path="/auth"
+          element={isAuth ? <Navigate to="/" /> : <Auth />}
+        ></Route>
+
+        <Route
+          path="/orders"
+          element={
+            <ProtectedRoutes>
+              <Orders />
+            </ProtectedRoutes>
+          }
+        ></Route>
+
+        <Route
+          path="/tables"
+          element={
+            <ProtectedRoutes>
+              <Tables />
+            </ProtectedRoutes>
+          }
+        ></Route>
+
+        <Route
+          path="/menu"
+          element={
+            <ProtectedRoutes>
+              <Menu />
+            </ProtectedRoutes>
+          }
+        ></Route>
+
         <Route path="*" element={<div>Not Found</div>}></Route>
       </Routes>
     </>
   );
+}
+
+function ProtectedRoutes({ children }) {
+  const { isAuth } = useSelector((state) => state.user);
+  if (!isAuth) {
+    return <Navigate to="/auth" />;
+  }
+  return children;
 }
 
 function App() {
---
+++ pos-frontend/src/hooks/useLoadData.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { getUserData } from "../https";
+import { removeUser, setUser } from "../redux/slices/userSlice";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+const useLoadData = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchuser = async () => {
+      try {
+        const { data } = await getUserData();
+        console.log(data);
+        const { _id, name, email, phone, role } = data.data;
+        dispatch(setUser({ _id, name, email, phone, role }));
+      } catch (error) {
+        dispatch(removeUser());
+        navigate("/auth");
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchuser();
+  }, [dispatch, navigate]);
+
+  return isLoading;
+};
+
+export default useLoadData;
